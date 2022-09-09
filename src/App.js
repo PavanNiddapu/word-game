fa2bb3368071e13f01bffab5ba5179ff24a53b0b
Lalith Niddapu <lalith.niddapu@thoughtworks.com>
2022-09-09 17:14:01 +0530

    Lalith | Show all letter in a word

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Letter from './component/Letter';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const aWord = "Hello";
+  return (<div>
+    {aWord.split("").map(letter => <Letter letter={letter} />)}
+  </div>);
 }
 
 export default App;
+// Create a box and a letter in it with big font at the center of the page -Done
+// Given N letter word, show each leter in N seperate boxes
+// Flash a word on top for 5 sec 
+// Type the word in the box and click on submit
+// Validate the word with the flashed word
+// Take a word at random from an array of words and do the above process
+// have proper coloring
